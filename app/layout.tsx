9fee49267d7a32dc9bf3b261567888f5376323ba
huybuivuanh <huy_buivu12345@yahoo.ca>
2026-08-23 18:50:18 -0600

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { AppModal } from "@/components/AppModal";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicon.png",
+  },
+};
 
 export default function RootLayout({
   children,
